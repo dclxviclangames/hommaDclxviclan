c9450124f16733e91638236f902165ccb8290494
dclxviclangames <135003207+dclxviclangames@users.noreply.github.com>
2023-07-13 20:32:45 +0000

    Update

---
+++ dclxviclancrack.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main ()
+{
+  int password;
+
+    printf("Enter password: ");
+    scanf("%d",&password);
+
+  if(password==0)
+  {
+      printf("Nice, Please subscribe on #dclxviclan");
+  }
+  else
+  {
+      printf("Sorry, Please subscribe on #dclxviclan");
+  }
+}
